2b78ac90cfcb0b3294401a871149f46f68cdea73
S K <sam@baremetalnetworks.com>
2021-04-19 15:08:44 -0600

    working expression detection

--- script.js
+++ script.js
@@ -38,6 +38,9 @@ video.addEventListener('play', () => {
                 displaySize)
             canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
                 faceapi.draw.drawDetections(canvas, resizedDetections)
+                faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+                faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+
     }, 100)
     
 })
